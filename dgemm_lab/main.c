3c08401be6b96adb5bb392c89e76c2a3ec9e7f97
Evangelia Kasapaki <evka@n-62-24-8.(none)>
2012-01-04 18:13:33 +0100

    block matrix multiplication

--- dgemm_lab/main.c
+++ dgemm_lab/main.c
@@ -8,7 +8,7 @@ void print_timing(double time_simple, double time_dgemm, int loopcount) {
     printf("%f %f \n", time_simple/loopcount, time_dgemm/loopcount);
 }
 
-void run_matrix_calc(int m, int n, int k, int loopcount) {
+void run_matrix_calc(int m, int n, int k, int loopcount, int s) {
     double** A = create_matrix(m, k);
     double** B = create_matrix(k, n);
     init_matrix(m, k, 10, A);
@@ -19,6 +19,7 @@ void run_matrix_calc(int m, int n, int k, int loopcount) {
 
     double simple_mm_time = 0;
     double gemm_mm_time = 0;
+	double block_mm_time = 0;
     
     for(int i = 0; i < loopcount; i++) {
         double** C = create_matrix(m, n);
@@ -29,18 +30,25 @@ void run_matrix_calc(int m, int n, int k, int loopcount) {
         gemm_mm_time += dgemm_mm(m, n, k, A, B, C);
         print_matrix(C, m, n, "C after dgemm_mm");
         print_timing(simple_mm_time, gemm_mm_time, i + 1);
+
+		C = create_matrix(m, n);
+        block_mm_time += block_mm(m, n, k, A, B, C, s);
+        print_matrix(C, m, n, "C after block_mm");
+        //print_timing(simple_mm_time, gemm_mm_time, i + 1);
     }
 }
 
 int main(int argc, char** argv) {
-    debug = 0;
+    debug = 1;
     
-    int m = 1000;
-    int n = 1000;
-    int k = 1000;
-    int loop_count = 100;
+    int m = 6;
+    int n = 6;
+    int k = 4;
+    int loop_count = 1;
 
-    run_matrix_calc(m, n, k, loop_count);
+	int s = 2;
+
+    run_matrix_calc(m, n, k, loop_count, s);
     
     return 0;
 }
--- dgemm_lab/matrix.h
+++ dgemm_lab/matrix.h
@@ -10,3 +10,5 @@ double** create_matrix(int row, int column);
 
 double simple_mm(int m, int n, int k, double** a, double** b, double** c);
 double dgemm_mm(int m, int n, int k, double** a, double** b, double** c);
+double block_mm(int m, int n, int k, double** a, double** b, double** c, int s);
+
