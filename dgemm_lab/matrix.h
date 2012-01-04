6f8fbf6c9768e50e0a9dfd0d0c139597b32c16f2
Henning Weiss <hdweiss@gmail.com>
2012-01-04 12:19:58 +0100

    Added timer functions.

--- dgemm_lab/matrix.h
+++ dgemm_lab/matrix.h
@@ -2,10 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sunperf.h>
+#include "xtime.h"
 
 void print_matrix(double** matrix, int row, int column, const char* message);
 void init_matrix(int row, int column, int scale_factor, double** matrix);
 double** create_matrix(int row, int column);
 
-double** simple_mm(int m, int n, int k, double** a, double** b);
-double** dgemm_mm(int m, int n, int k, double** a, double** b);
+double simple_mm(int m, int n, int k, double** a, double** b, double** c);
+double dgemm_mm(int m, int n, int k, double** a, double** b, double** c);
