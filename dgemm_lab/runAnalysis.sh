b9159557265fdcd869271659c92fd6c7ab13d209
Henning Weiss <hdweiss@gmail.com>
2012-01-05 17:44:58 +0100

    Created data extraction script.

--- dgemm_lab/runAnalysis.sh
+++ dgemm_lab/runAnalysis.sh
@@ -9,13 +9,3 @@ for m in `seq 10 100 1000`
 do
     /opt/oracle/solstudiodev/lib/analyzer/lib/../../../bin/collect -o $EXP_NAME.$m.er -p on -h PAPI_fp_ops,on,l2dm,on,dcm,on,cycles,on -S on -A on ./mm $m $m $m 
 done
-
-
-for file in `find $EXP_NAME* -maxdepth 0  -printf "%p$IFS" `
-do
-    echo $file
-    er_print -lines $file/ |grep simple_mm; #| awk {'print $4'};
-#    er_print -lines $file/ |grep simple_mm; #| awk {'print $4'};
-	
-done
-
