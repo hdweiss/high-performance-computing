cbe66eb698cb7c6257d3d90bc4a1acd2cab7c442
Henning Weiss <hdweiss@gmail.com>
2012-01-05 18:14:10 +0100

    Fixed plotting.

--- dgemm_lab/plot.p
+++ dgemm_lab/plot.p
@@ -1,7 +1,10 @@
 set autoscale
-set title "Simple vs. DGEMM"
 set out 'matrix_graph.pdf'
+
+set title "Simple vs. DGEMM"
 set xlabel "Matrix Size/KB"
 set ylabel "FLOP"
-plot  "measure.dat" using 1:2 title 'Simple' w linespoints, \
-      "measure.dat" using 1:3 title 'DGEMM' w linespoints
+
+plot  "measure.dat" using 1:2 title 'Simple_mm' w linespoints, \
+      "measure.dat" using 1:4 title 'Block_mm' w linespoints, \
+      "measure.dat" using 1:3 title 'DGEMM_mm' w linespoints
--- dgemm_lab/runPlotData.sh
+++ dgemm_lab/runPlotData.sh
@@ -2,10 +2,11 @@
 
 EXP_FILES='experiments/hpc*'
 FIND_COMMAND='find experiments/hpc* -maxdepth 0  -printf "%p$IFS"'
+OUTPUT_FILE='measure.dat'
 
 for file in $EXP_FILES
 do
-    echo $file | awk -F. {' printf "%i ", $2 '} 
+    echo $file | awk -F. {' printf "%i ", $2^2*3*8/1024 '};
     er_print -func $file 2> /dev/null | grep simple_mm | awk {' time=$2; flop=$4; printf "%.1f " , flop/time/1024/1024'};
     er_print -func $file 2> /dev/null | grep dgemm_mm | awk {' time=$2; flop=$4; printf "%.1f " , flop/time/1024/1024'};
     er_print -func $file 2> /dev/null | grep block_mm | awk {' time=$2; flop=$4; printf "%.1f " , flop/time/1024/1024'};
