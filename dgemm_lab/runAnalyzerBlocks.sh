045304e8ae7a9f0d00520862c832fd95bf9c7235
Henning Weiss <hdweiss@gmail.com>
2012-01-06 01:47:26 +0100

    Added readme file, wrote some more on report

--- dgemm_lab/runAnalyzerBlocks.sh
+++ dgemm_lab/runAnalyzerBlocks.sh
@@ -4,12 +4,12 @@ module swap studio/12u3b
 EXP_NAME=experiments/blocks
 COLLECT_CMD='/opt/oracle/solstudiodev/lib/analyzer/lib/../../../bin/collect'
 
-BLOCK_SIZE=32
+MATRIX_SIZE=100
 
 rm -Rf $EXP_NAME.*
 
-for m in `seq 1 50`
+for blocks in `seq 1 50`
 do
-    $COLLECT_CMD -o $EXP_NAME.$m.er -p on -h PAPI_fp_ops,on,l2dm,on,dcm,on,cycles,on -S on -A on \
-        ./mm 500 500 500 $m
+    $COLLECT_CMD -o $EXP_NAME.$blocks.er -p on -h PAPI_fp_ops,on,l2dm,on,dcm,on,cycles,on -S on -A on \
+        ./mm $MATRIX_SIZE $MATRIX_SIZE $MATRIX_SIZE $blocks
 done
