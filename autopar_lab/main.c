b2b6d043256d143127678ddc3c9f46c4661f0552
Henning Weiss <hdweiss@gmail.com>
2012-01-09 15:24:46 +0100

    made it compilable.

--- autopar_lab/main.c
+++ autopar_lab/main.c
@@ -1,24 +1,28 @@
 
 #include <stdio.h>
+
 #ifdef _OPENMP
 #include <omp.h>
 #endif
 
+#include "integrand.h"
 
-int main(int argc, char** argv) {
-    int n;
-
+void run_f(int n) {
     double h = 1.0 / (double)n;
     double sum = 0.0;
 
 #pragma omp parallel for default(none) \
     shared(n,h) private(i,x)           \
     reduction(+: sum)
-
     for(int i=1; i<=n; i++) {
         double x = h * ((double)i + 0.5);
         sum += f(x);
     } /* end parallel */
-    
+
+}
+
+
+int main(int argc, char** argv) {
+    run_f(0);
 }
 
