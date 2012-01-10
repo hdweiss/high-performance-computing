a74ec1a98e4cf3913585941ba46d4b969794a013
Evangelia Kasapaki <evka@n-62-24-6.(none)>
2012-01-10 15:15:34 +0100

    poisson directory

---
+++ poisson_lab/jacobi.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+double f(int x, int y) {
+	if ( (x >= 0) && (x <= 1/3) && (y >= -2/3) && (y <= ))
+}
+
+void jacobi(int n, double *grid/*, int kmax*/) {
+
+	double *new_grid;
+	//init();
+	//for (int k = 0; k < kmax; k++) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < 0; j++) {
+				grid[(i-1)*n+j]
+				grid[(i+1)*n+j]
+				grid[i*n+j-1]
+				grid[i*n+j+1]
+				new_grid[i*n+j] = grid[(i-1]
+			}
+		}
+	//}
+}
+
